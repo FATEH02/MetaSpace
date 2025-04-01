421545aea1a7fb31e385934f45572ba9f094710f
FATEH07 <fatehsingh07462@gmail.com>
2025-04-01 21:50:48 +0530

    added process

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const origin = "https://metaspacefrontend-fateh02s-projects.vercel.app";
+  const origin =  process.env.NEXT_PUBLIC_SITE_URL ;
 
   if (code) {
     const supabase = createClient();
